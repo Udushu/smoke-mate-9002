26e5524d6b62f5860e77edddbc7e9713e7bdc075
Oleg Sosnovski <OSosnovski@gastops.com>
2025-06-02 11:53:51 -0400

    First implementation of the bangbang controller

---
+++ firmware/src/bangbang.cpp
@@ -0,0 +1,58 @@
+#include "bangbang.h"
+
+BangBang::BangBang(int low, int high, int hyst)
+    : m_lowThreshold(low), m_highThreshold(high), m_hysteresis(hyst), m_state(BANGBANG_STATE_IDLE)
+{
+}
+
+void BangBang::setThresholds(int low, int high)
+{
+    m_lowThreshold = low;
+    m_highThreshold = high;
+}
+
+void BangBang::setHysteresis(int hyst)
+{
+    m_hysteresis = hyst;
+}
+
+BANGBANG_STATE BangBang::service(int currentTemp)
+{
+
+    switch (m_state)
+    {
+    case BANGBANG_STATE_IDLE:
+        if (currentTemp > m_highThreshold)
+        {
+            m_state = BANGBANG_STATE_COOL;
+        }
+        else if (currentTemp < m_lowThreshold)
+        {
+            m_state = BANGBANG_STATE_HEAT;
+        }
+        else
+        {
+            m_state = BANGBANG_STATE_IDLE;
+        }
+        break;
+    case BANGBANG_STATE_HEAT:
+        if (currentTemp >= (m_lowThreshold + m_hysteresis))
+        {
+            m_state = BANGBANG_STATE_IDLE; // Stop heating if above low threshold plus hysteresis
+        }
+        break;
+    case BANGBANG_STATE_COOL:
+        if (currentTemp <= (m_highThreshold - m_hysteresis))
+        {
+            m_state = BANGBANG_STATE_IDLE; // Stop cooling if below high threshold minus hysteresis
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+BANGBANG_STATE BangBang::getState() const
+{
+    return m_state;
+}
---
+++ firmware/src/bangbang.h
@@ -0,0 +1,31 @@
+#ifndef BANGBANG_H
+#define BANGBANG_H
+
+#include "types.h"
+
+enum BANGBANG_STATE
+{
+    BANGBANG_STATE_IDLE,
+    BANGBANG_STATE_HEAT,
+    BANGBANG_STATE_COOL
+};
+
+class BangBang
+{
+private:
+    int m_lowThreshold;     // Lower threshold (F)
+    int m_highThreshold;    // Upper threshold (F)
+    int m_hysteresis;       // Hysteresis (F)
+    BANGBANG_STATE m_state; // Current state
+public:
+    BangBang(int low, int high, int hyst);
+
+    void setThresholds(int low, int high);
+    void setHysteresis(int hyst);
+
+    // Returns the current state (IDLE, HEAT, COOL)
+    BANGBANG_STATE service(int currentTemp);
+    BANGBANG_STATE getState() const;
+};
+
+#endif // BANGBANG_H
