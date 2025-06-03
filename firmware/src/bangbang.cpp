c3868cbae9c457828096fd74bed085baaebf28f0
Oleg Sosnovski <OSosnovski@gastops.com>
2025-06-03 11:03:39 -0400

    Added debug outputs to the BangBang class

--- firmware/src/bangbang.cpp
+++ firmware/src/bangbang.cpp
@@ -18,6 +18,21 @@ void BangBang::setHysteresis(int hyst)
 
 BangBangState BangBang::service(int currentTemp, ulong currentTimeMSec)
 {
+#ifdef DEBUG_BANGBANG
+    DEBUG_PRINTLN("BangBang::service() - Enter");
+    // Print the current state, thresholds, and temperature
+    DEBUG_PRINT("---> Current State: ");
+    convertBangBangStateToString();
+    DEBUG_PRINT("---> Low Threshold: ");
+    DEBUG_PRINTLN(m_lowThreshold);
+    DEBUG_PRINT("---> High Threshold: ");
+    DEBUG_PRINTLN(m_highThreshold);
+    DEBUG_PRINT("---> Hysteresis: ");
+    DEBUG_PRINTLN(m_hysteresis);
+    DEBUG_PRINT("---> Current Temperature: ");
+    DEBUG_PRINTLN(currentTemp);
+
+#endif
 
     switch (m_state)
     {
@@ -35,19 +50,48 @@ BangBangState BangBang::service(int currentTemp, ulong currentTimeMSec)
             m_state = BANGBANG_STATE_IDLE;
         }
         break;
+
     case BANGBANG_STATE_HEAT:
         if (currentTemp >= (m_lowThreshold + m_hysteresis))
         {
             m_state = BANGBANG_STATE_IDLE; // Stop heating if above low threshold plus hysteresis
         }
         break;
+
     case BANGBANG_STATE_COOL:
         if (currentTemp <= (m_highThreshold - m_hysteresis))
         {
             m_state = BANGBANG_STATE_IDLE; // Stop cooling if below high threshold minus hysteresis
         }
         break;
+
     default:
+        break;
+    }
+
+#ifdef DEBUG_BANGBANG
+    DEBUG_PRINTLN("BangBang::service() - Exit");
+#endif
+
+    return m_state;
+}
+
+void BangBang::convertBangBangStateToString()
+{
+    switch (m_state)
+    {
+    case BANGBANG_STATE_IDLE:
+        DEBUG_PRINTLN("IDLE");
+        break;
+    case BANGBANG_STATE_HEAT:
+        DEBUG_PRINTLN("HEAT");
+        break;
+    case BANGBANG_STATE_COOL:
+
+        DEBUG_PRINTLN("COOL");
+        break;
+    default:
+        DEBUG_PRINTLN("UNKNOWN");
         break;
     }
 }
--- firmware/src/bangbang.h
+++ firmware/src/bangbang.h
@@ -1,7 +1,11 @@
 #ifndef BANGBANG_H
 #define BANGBANG_H
 
+#include <Arduino.h>
 #include "types.h"
+#include "debug.h"
+
+#define DEBUG_BANGBANG
 
 enum BangBangState
 {
@@ -17,6 +21,9 @@ private:
     int m_highThreshold;   // Upper threshold (F)
     int m_hysteresis;      // Hysteresis (F)
     BangBangState m_state; // Current state
+
+    void convertBangBangStateToString();
+
 public:
     BangBang(int low, int high, int hyst);
 
