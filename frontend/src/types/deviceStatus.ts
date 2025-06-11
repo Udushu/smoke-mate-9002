bd7fb64fd6465fc4170e4b2c112a82683de4d376
Udushu <o.sosnovski@gmail.com>
2025-06-10 22:40:13 -0400

    Support new DeviceStatus definition with temperature profile in the front end

--- frontend/src/types/deviceStatus.ts
+++ frontend/src/types/deviceStatus.ts
@@ -1,39 +1,49 @@
 export interface DeviceStatus {
-    timeStamp: Date;
-    isConnected: boolean;
-    isRunning: boolean;
-    controllerStartMSec: number;
-    uptime: number;
-    temperatureFood: number;
-    temperatureSmoker: number;
-    temperatureTarget: number;
-    doorPosition: number;
-    fanPWM: number;
-    isWiFiConnected: boolean;
-    ipAddress: string;
-    RSSI: number;
-    bars: number;
-    uuid: string;
-    networkName: string;
-    temperatureError: number;
+  timestamp: string | null;
+  isConnected: boolean;
+  isRunning: boolean;
+  uuid: string;
+  uptime: number;
+  controllerStartMSec: number;
+  temperatureSmoker: number;
+  temperatureFood: number;
+  temperatureTarget: number;
+  fanPWM: number;
+  doorPosition: number;
+  RSSI: number;
+  bars: number;
+  ipAddress: string;
+  isWiFiConnected: boolean;
+  networkName: string;
+  temperatureError: number;
+  isProfileRunning: string;
+  temperatureProfileStepIndex: number;
+  temperatureProfileStartTimeMSec: number;
+  temperatureProfileStepsCount: number;
+  temperatureProfileStepType: string;
 }
 
 export const defaultDeviceStatus: DeviceStatus = {
-    timeStamp: new Date(),
-    isConnected: false,
-    isRunning: false,
-    controllerStartMSec: 0,
-    uptime: 0,
-    temperatureFood: 0,
-    temperatureSmoker: 0,
-    temperatureTarget: 0,
-    doorPosition: 0,
-    fanPWM: 0,
-    isWiFiConnected: false,
-    ipAddress: "",
-    RSSI: 0,
-    bars: 0,
-    uuid: "",
-    networkName: "",
-    temperatureError: 0,
+  timestamp: null,
+  isConnected: false,
+  isRunning: false,
+  uuid: '',
+  uptime: 0,
+  controllerStartMSec: 0,
+  temperatureSmoker: 0,
+  temperatureFood: 0,
+  temperatureTarget: 0,
+  fanPWM: 0,
+  doorPosition: 0,
+  RSSI: 0,
+  bars: 0,
+  ipAddress: '',
+  isWiFiConnected: false,
+  networkName: '',
+  temperatureError: 0,
+  isProfileRunning: 'UNKNOWN',
+  temperatureProfileStepIndex: -1,
+  temperatureProfileStartTimeMSec: 0,
+  temperatureProfileStepsCount: 0,
+  temperatureProfileStepType: 'UNKNOWN',
 };
